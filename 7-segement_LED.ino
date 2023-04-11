3944c1eca5d4a94108cd09e1fa72fd057d9814db
Sockalingam27 <90521489+Sockalingam27@users.noreply.github.com>
2023-04-11 21:36:43 +0530

    Pause button update
    Using the blue button aka pause_play (pin no. 7) the stopwatch can be paused and it can be played by clicking the yellow button aka set button (pin no. 8)

--- 7-segement_LED.ino
+++ 7-segement_LED.ino
@@ -7,6 +7,7 @@ Adafruit_7segment matrix = Adafruit_7segment();
 int plus = 2;
 int minus = 4;
 int set = 8;
+int pause_play = 7;
 int buzzer = 9;
 int counter =0;
 unsigned long A;
@@ -38,6 +39,7 @@ matrix.begin(0x70);
 pinMode(plus, INPUT_PULLUP);
 pinMode(set, INPUT_PULLUP);
 pinMode(minus, INPUT_PULLUP);
+pinMode(pause_play, INPUT_PULLUP);
 pinMode(buzzer,OUTPUT);
 pinMode(LED_BUILTIN, OUTPUT);
 
@@ -49,21 +51,22 @@ matrix.print("HELO");
 
 void loop() {
 Serial.println(counter);
-  int buttonValue1 = digitalRead(plus);
-   if (buttonValue1 == LOW){
+int buttonValue1 = digitalRead(plus);
+   if (buttonValue1 == LOW)
+   {
      delay(300);
      counter+=15;
      Print_time(counter);
      
      }
 int buttonValue2 = digitalRead(minus);
-   if (buttonValue2 == LOW){
+   if (buttonValue2 == LOW)
+   {
      delay(300);
      counter-=15;
      Print_time(counter);
    }
-
-   int buttonVal3 = digitalRead(set);
+int buttonVal3 = digitalRead(set);
    if (buttonVal3 == LOW)
    {
         counter--;
@@ -73,6 +76,19 @@ int buttonValue2 = digitalRead(minus);
           A = millis();
           while(((B-A)/1000)!=1)
           {
+            int buttonValue4 = digitalRead(pause_play);
+            if (buttonValue4 == LOW)
+            {
+              //Print_time(360);
+              int wait_var = 0;
+              while(wait_var == 0)
+              { 
+                A = millis();
+                buttonValue4 = digitalRead(set);
+                if (buttonValue4 == LOW)
+                {wait_var =1;}
+              }
+            }
             B = millis();
           }
           counter--;
